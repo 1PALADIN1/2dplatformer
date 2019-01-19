8dcaf908f13a84d7fe15f90a4a0b5d3774f2cd67
1PALADIN1 <sonikx94@yandex.ru>
2019-01-19 12:43:24 +0300

    Добавил перезагрузку сцены

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -18,6 +17,12 @@ public class GameController : MonoBehaviour
 		if (Input.GetKey(KeyCode.Q))
         {
             Application.Quit();
+        }
+
+        //перезагрузка сцены
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 	}
 }
