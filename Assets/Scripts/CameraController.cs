f360fff87b5973eff389b2cbdb22ad756f630432
1PALADIN1 <sonikx94@yandex.ru>
2019-01-19 12:11:07 +0300

    Настроил перемещение камеры

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject watchObject;          //объект, за которым должна следить камера
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (watchObject != null)
+        {
+            transform.position = new Vector3(watchObject.transform.position.x, watchObject.transform.position.y, transform.position.z);
+        }
+	}
+}
