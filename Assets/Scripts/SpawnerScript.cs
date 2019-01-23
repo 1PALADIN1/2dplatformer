f8d9cc7f94455110f204883c255008cc0f602051
1PALADIN1 <sonikx94@yandex.ru>
2019-01-23 19:42:10 +0300

    Добавил спауны

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject spawnObject;
+
+	void Start () {
+        Spawn();
+	}
+
+    public void Spawn()
+    {
+        if (spawnObject != null)
+            Instantiate(spawnObject, transform);
+    }
+}
