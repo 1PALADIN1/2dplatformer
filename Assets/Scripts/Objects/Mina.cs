a2a8db9cb284e1de137acfc16d8904b98555ae51
1PALADIN1 <sonikx94@yandex.ru>
2019-01-27 18:05:53 +0300

    Добавил мину, которую создаёт игрок

---
+++ Assets/Scripts/Objects/Mina.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mina : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 5;                         //урон
+    [SerializeField]
+    private float pushForce = 10.0f;                //сила отталкивания
+    [SerializeField]
+    private float delay = 2.0f;                     //задержка при создании мины (активируется только через это время)
+
+    private BoxCollider2D boxCollider;              //коллайдер для мины (если игрок/противник наступит)
+    private CircleCollider2D circleCollider;        //радиус поражения
+    
+	void Start ()
+    {
+        circleCollider = gameObject.GetComponent<CircleCollider2D>();
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+	}
+
+    /// <summary>
+    /// Обработка взрыва (мина активировалась)
+    /// </summary>
+    /// <param name="collision">Объект Collision2D того, кто находится в радиусе поражения</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player")
+        {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            Health health = collision.GetComponent<Health>();
+            if (rb != null)
+            {
+                //обнуляем скорость перемещения
+                rb.velocity = Vector2.zero;
+                //отталкиваем объекты
+                rb.AddForce((collision.transform.position - transform.position).normalized * pushForce, ForceMode2D.Impulse);
+            }
+            //наносим урон
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Проверка, кто наступил на мину
+    /// </summary>
+    /// <param name="collision">Объект Collision2D того, кто наступил</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player")
+        {
+            circleCollider.enabled = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerStrike.cs
+++ Assets/Scripts/PlayerStrike.cs
@@ -7,9 +7,13 @@ public class PlayerStrike : MonoBehaviour {
     [SerializeField]
     private GameObject bullet;          //пуля
     [SerializeField]
+    private GameObject mina;            //мина
+
+    [SerializeField]
     private Transform startPosition;    //откуда будут появлятся пули
     
 	void Update () {
+        //выстрелы
 		if (Input.GetKeyDown(KeyCode.J) || Input.GetMouseButtonDown(0))
         {
             if (bullet != null)
@@ -17,7 +21,15 @@ public class PlayerStrike : MonoBehaviour {
                 var b = Instantiate(bullet, startPosition.position, Quaternion.identity);
                 b.GetComponent<BulletController>().direction = gameObject.GetComponent<PlayerMove>().Direction;
             }
-                
+        }
+
+        //мина
+        if (Input.GetKeyDown(KeyCode.I) || Input.GetMouseButtonDown(1))
+        {
+            if (mina != null)
+            {
+                Instantiate(mina, startPosition.position, Quaternion.identity);
+            }
         }
 	}
 }
