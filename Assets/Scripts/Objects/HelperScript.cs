a588c293e8314036e491e0c3c2e380aed0a54861
1PALADIN1 <sonikx94@yandex.ru>
2019-02-05 21:32:13 +0300

    Добавил на уровень подсказки на основе GUI

---
+++ Assets/Scripts/Objects/HelperScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HelperScript : MonoBehaviour
+{
+    [TextArea]
+    [SerializeField]
+    private string _textMessage;        //текст, который будет отображаться на панели
+    [SerializeField]
+    private Text _textComponent;        //компонента с текстом из канваса
+    [SerializeField]
+    private Canvas _canvas;             //сам канвас
+    [SerializeField]
+    private float _touchRadius;         //радиус столкновения с игроком (внутри него становится видимым канвас)
+    [SerializeField]
+    private LayerMask _layerMask;       //с кем сталкиваясь отображаем подсказки
+
+    private RaycastHit2D _raycastHit2D;
+
+	private void Start ()
+    {
+        _textComponent.text = _textMessage;
+	}
+
+    private void FixedUpdate()
+    {
+        //проверяем столкновение с игроком
+        _raycastHit2D = Physics2D.CircleCast(transform.position, _touchRadius, Vector2.right, 0.0f, _layerMask);
+
+        //если игрок в поле зрения
+        if (_raycastHit2D.collider != null)
+        {
+            //активируем канвас
+            if (!_canvas.enabled)
+            {
+                _canvas.enabled = true;
+                _textComponent.resizeTextForBestFit = true;
+                _textComponent.resizeTextForBestFit = false;
+            }
+        }
+        else
+        {
+            //в противном случае прячем канвас
+            if (_canvas.enabled)
+                _canvas.enabled = false;
+        }
+    }
+}
