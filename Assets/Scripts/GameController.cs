462640b6f8c7f0f5e3b27668282a2ac38c7f5466
1PALADIN1 <sonikx94@yandex.ru>
2019-02-06 21:30:29 +0300

    Сделал меню паузы

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,26 +3,86 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _menu;               //менюшка паузы
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        //выход из игры
-		if (Input.GetKey(KeyCode.Q))
-        {
-            Application.Quit();
-        }
+    private float _backupScaleTime;         //сохраняем таймскейл
+    private PlayerMove _playerMove;         //компонента перемещения игрока
 
-        //перезагрузка сцены
-        if (Input.GetKey(KeyCode.R))
+    /// <summary>
+    /// На паузе ли игра (свойство)
+    /// </summary>
+    private bool GamePaused
+    {
+        get
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            return Time.timeScale == 0;
         }
-	}
+    }
+
+    private void Start()
+    {
+        _backupScaleTime = Time.timeScale;
+        _playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            ExitGame();
+
+        if (Input.GetKeyDown(KeyCode.R))
+            ReloadLevel();
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 0)
+                ContinueGame();
+            else
+                PauseGame();
+        }
+    }
+
+    /// <summary>
+    /// Перезагрузка сцены
+    /// </summary>
+    public void ReloadLevel()
+    {
+        if (GamePaused) Time.timeScale = _backupScaleTime;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Выход из игры
+    /// </summary>
+    public void ExitGame()
+    {
+        if (GamePaused) Time.timeScale = _backupScaleTime;
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Ставит игру на паузу
+    /// </summary>
+    public void PauseGame()
+    {
+        if (Time.timeScale != 0)
+        {
+            _backupScaleTime = Time.timeScale;
+            //останавилваем время
+            Time.timeScale = 0;
+            if (_menu != null) _menu.SetActive(true);
+            if (_playerMove != null) _playerMove.BlockControl = true;
+        }
+    }
+
+    /// <summary>
+    /// Продолжение игры
+    /// </summary>
+    public void ContinueGame()
+    {
+        Time.timeScale = _backupScaleTime;
+        if (_menu != null) _menu.SetActive(false);
+        if (_playerMove != null) _playerMove.BlockControl = false;
+    }
 }
