ca5b1577fb16db5859f0f4521c2ba0f2ea9b0e64
1PALADIN1 <sonikx94@yandex.ru>
2019-02-06 23:28:51 +0300

    Добавил переходы между уровнями

---
+++ Assets/Scripts/Other/FinishLevelScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FinishLevelScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _gameController;
+    [SerializeField]
+    private string _nextLevel;              //следующий уровень
+
+    private GameController _controller;
+
+	private void Start ()
+    {
+        //методы управления игровым процессом
+        if (_gameController != null)
+            _controller = _gameController.GetComponent<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //если игрок дошёл до финиша
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if (_controller != null)
+            {
+                if (_nextLevel.Equals("Finish")) return;
+                _controller.GotoLevel(_nextLevel);
+            }
+        }
+    }
+}
